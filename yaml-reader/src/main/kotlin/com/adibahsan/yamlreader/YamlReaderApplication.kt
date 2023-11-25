3a97a35f6473722c963cd85262f916c7c6d9f804
Adib <adibahsan@iut-dhaka.edu>
2023-11-26 02:14:41 +0600

    feat(yaml-reader): project initiated

---
+++ yaml-reader/src/main/kotlin/com/adibahsan/yamlreader/YamlReaderApplication.kt
@@ -0,0 +1,11 @@
+package com.adibahsan.yamlreader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YamlReaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<YamlReaderApplication>(*args)
+}
---
+++ yaml-reader/src/test/kotlin/com/adibahsan/yamlreader/YamlReaderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.adibahsan.yamlreader
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YamlReaderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
